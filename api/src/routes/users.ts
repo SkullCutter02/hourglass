9c2ed8e890a6790fc2b6c72a2bb3a09770015e0e
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-02-26 21:19:56 +0800

    Created route to check whether user is an admin in a project or not

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,6 +1,7 @@
 import { Router, Request, Response } from "express";
 
 import User from "../entity/User";
+import Project from "../entity/Project";
 import verifyToken from "../middleware/verifyToken";
 import { AuthDataType } from "../types/AuthDataType";
 
@@ -14,6 +15,27 @@ router.get("/projects", verifyToken(), async (req: Request, res: Response) => {
       { relations: ["projectMembers", "projectMembers.project", "projectMembers.project.categories"] }
     );
     return res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ msg: "Something went wrong" });
+  }
+});
+
+router.get("/admin/:projectUuid", verifyToken(), async (req: Request, res: Response) => {
+  try {
+    const { projectUuid } = req.params;
+    const authData: AuthDataType = res.locals.authData;
+
+    const project = await Project.findOneOrFail(
+      { uuid: projectUuid },
+      { relations: ["projectMembers", "projectMembers.user"] }
+    );
+
+    const isAdmin = project.projectMembers.some(
+      (projectMember) => projectMember.user.uuid === authData.uuid && projectMember.role === "admin"
+    );
+
+    return res.json(isAdmin);
   } catch (err) {
     console.log(err);
     return res.status(500).json({ msg: "Something went wrong" });
