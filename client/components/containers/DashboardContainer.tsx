a0490a0094b3811ca2074a033857b401d3fbe650
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2021-03-03 20:09:14 +0800

    Added create project button in main dashboard page

--- client/components/containers/DashboardContainer.tsx
+++ client/components/containers/DashboardContainer.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 
+import ArrowButton from "../reusable/ArrowButton";
+
 const DashboardContainer: React.FC = () => {
   return (
     <React.Fragment>
       <div className="dashboard-container">
         <h1>Welcome to your Dashboard!</h1>
+        <div className="arrow-btn-container">
+          <ArrowButton
+            text={"Create new Project"}
+            buttonColor={"#658aa2"}
+            buttonHoverColor={"#526c7c"}
+            textColor={"#fff"}
+            textSize={11}
+            link={"/dashboard/project/create"}
+          />
+        </div>
       </div>
 
       <style jsx>{`
         .dashboard-container {
-          padding: 30px 50px;
+          padding: 40px 50px;
+        }
+
+        h1 {
+          cursor: initial;
+        }
+
+        .arrow-btn-container {
+          margin: 40px 0;
         }
       `}</style>
     </React.Fragment>
